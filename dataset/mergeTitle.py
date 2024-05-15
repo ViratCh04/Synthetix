1d49aeaa0b0183a1c254169360834f164f52542a
ViratCh04 <105713825+ViratCh04@users.noreply.github.com>
2024-05-15 17:24:16 +0530

    teded dataset �

--- dataset/mergeTitle.py
+++ dataset/mergeTitle.py
@@ -2,14 +2,25 @@ import pandas as pd
 
 captions = pd.read_csv("final_captions.csv")
 titles = pd.read_excel("titles.xlsx")
+
 titles.columns = ['Title', 'Link']
-good = {}
 
-for url1 in captions['Link']:
+matched_data = []
+
+for i, url1 in enumerate(captions['Link']):
     video_url = "https://www.youtube.com/watch?v=" + url1
-    for url2 in titles['Link']:
-        if row 
+    for j, url2 in enumerate(titles['Link']):
+        if video_url == url2:
+            matched_data.append({
+                'Title': titles['Title'][j],
+                'Link': video_url,
+                'Caption': captions['Caption'][i]
+            })
+            #print(titles['Title'][j])
+            #print(captions['Caption'][i])
+            #print()
+matched_df = pd.DataFrame(matched_data)
 
+matched_df.to_csv('teded.csv', index=False)
 
 print(titles.info())
-print(titles['Title'])
